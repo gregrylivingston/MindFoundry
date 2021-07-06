b59f8d6aa53337d47171bfe6235d211300522805
Gregry Livingston <livingsgb@gmail.com>
2021-07-06 13:51:59 -0700

    media demos

--- components/cardBuilder.js
+++ components/cardBuilder.js
@@ -14,20 +14,42 @@ img: string           file location of image
 
 
 
+var mediaHTML;
 
 
 function card(data){
+
+  switch (data.mediaType){
+    case "Image":
+          mediaHTML = "<img src='" + data.media + "'>";
+          console.log(mediaHTML);
+          break
+    case "iframe":
+          mediaHTML = data.media;
+          break
+    default:
+          mediaHTML = "";
+  }
+
     document.getElementById("cards").innerHTML+= `
       <div class="card">
+
         <div class="card-type">
           <div>${data.type} ${data.subtype}</div>
         </div>
+
         <div class="card-header">
-          <img class="card-header-img" src=${data.img}>
+          <img class="card-header-img" src=${data.icon}>
           <div class="card-title-group">
             <div class="card-title">${data.title}</div>
             <div class="card-subtitle">${data.subtitle}</div>
           </div>
+        </div>
+        <div class="media">${mediaHTML}
+        </div>
+        <div class="card-description">
+        </div>
+        <div class="card-footer">
         </div>
       </div>
     `
@@ -41,28 +63,37 @@ var sampleCards = [
       subtype:"Accepted",
       title:"Make a Dance Video",
       subtitle:"Difficulty: <img src='img/mf_logo.png' height='16px'><img src='img/mf_logo.png' height='16px'>",
-      img:"img/badges/dancing4.png"
+      icon:"img/badges/dancing4.png",
+      mediaType:"Image",
+      media:"img/badges/dancing4.png",
+      description:'',
+      footer:'',
+
     },
     {
       type:"Challenge",
       subtype:"Available",
       title:"Make a Dance Video",
       subtitle:"Difficulty: brains brains",
-      img:"img/badges/dancing4.png"
+      icon:"img/badges/dancing4.png",
+      mediaType:"iframe",
+      media:'  <iframe width="100%" src="https://www.youtube.com/embed/xL-XDbMMMSg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+      description:'',
+      footer:'',
     },
     {
       type:"Challenge",
       subtype:"Submitted",
       title:"Make a Dance Video",
       subtitle:"Difficulty: brains brains",
-      img:"img/badges/dancing4.png"
+      icon:"img/badges/dancing4.png"
     },
     {
       type:"Challenge",
       subtype:"Completed",
       title:"Make a Dance Video",
       subtitle:"Difficulty: brains brains",
-      img:"img/badges/dancing4.png"
+      icon:"img/badges/dancing4.png"
     },
 
 
