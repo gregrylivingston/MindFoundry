509e8b6a29fa19deea5c79d850e8c65e7a6b3c2b
Gregry Livingston <livingsgb@gmail.com>
2021-07-06 20:02:10 -0700

    events submenu updates

--- components/menu-events.js
+++ components/menu-events.js
@@ -2,32 +2,38 @@ document.write(
   `
   <div class="user-menu" id="menu-events" style="display:none;">
 
-      <div>
-        <button class="selectButton"> 2<img src="img/currency/token_league.png" height="64px">
+    <h1>Events</h1>
+      <div style="width:100%;vertical-align:middle;align-items:middle;">
+
+          <button class="selectButton"><span style="font-size:1.5em;display:inline;padding-top:-20px;">All Programs</span>
+          </button>
+        <button class="selectButton"><span style="font-size:3em;display:inline;padding-top:-20px;">2</span><img src="img/currency/token_league.png" height="64px">
         </button>
-        <button class="selectButton"> 1<img src="img/currency/token_tournament.png" height="64px">
+        <button class="selectButton"><span style="font-size:3em;display:inline;padding-top:-20px;">2</span><img src="img/currency/token_tournament.png" height="64px">
         </button>
         <button class="selectButton">
-          + <img src="img/menu/menu_events.png" height="64px">
+          <span style="font-size:3em;display:inline;padding-top:-20px;">+</span><img src="img/menu/menu_events.png" height="64px">
         </button>
       </div>
 
-      <button class="selectButton" style="width:30%;">
-        My Calendar
-      </button>
-      <button class="selectButton" style="width:30%;">
-        Events History
-      </button>
+    <div class="table">
 
+          <button class="table-filter-button">
+              <span style="font-size:1.5em;display:inline;">Calendar</span>
+          </button>
 
+          <button class="table-filter-button">
+              <span style="font-size:1.5em;display:inline;">Progress</span>
+          </button>
 
-      <div style="display:inline-block;width:45%;">
-        <button class="selectButton">
-          Select Program Vertical
-        </button>
-      </div>
+          <button class="table-filter-button">
+              <span style="font-size:1.5em;display:inline;">History</span>
+          </button>
 
-
+          <div class="table-body">
+          <br><br><br><br>
+          </div>
+    </div>
 
 
   </div>
