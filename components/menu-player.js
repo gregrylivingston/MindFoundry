6eab4ecca8e2588c58052b994fa173be426cb9df
Andrew <adllivings@gmail.com>
2021-07-06 21:03:34 -0600

    menu-player submenu items, related css

--- components/menu-player.js
+++ components/menu-player.js
@@ -1,7 +1,15 @@
 document.write(
   `
   <div class="user-menu" id="menu-player">
-      This is the player menu component
+      <button class="menuButton">
+        <img class="menuButton-image" src="img/avatar/avatar_cuteawil.png"><br>Spellbound
+      </button>
+      <button class="menuButton">
+        <img class="menuButton-image" src="img/avatar/avatar_girwil.png"><br>Sidekick
+      </button>
+      <button class="menuButton">
+      <img class="menuButton-image" src="img/avatar/avatar_rocketleague.png"><br>Ride
+      </button>
 
   </div>
   `
