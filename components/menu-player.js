51eef2d57e46013094e8b106193eec825ab93857
Andrew <adllivings@gmail.com>
2021-07-07 10:08:47 -0600

    userplayer buttons, club crest images

--- components/menu-player.js
+++ components/menu-player.js
@@ -1,16 +1,16 @@
 document.write(
   `
   <div class="user-menu" id="menu-player">
-      <button class="avatarButton" style="width:47%;font-size:18pt;display:inline-block;">
-        <p>Spellbound<img class="avatarButton-image" src="img/avatar/avatar_cuteawil.png" style="display:inline;horizontal-align:left;vertical-align:middle;display:inline;
-        padding:3%;align:right;"></p>
+      <button class="avatarButton" style="width:30%;font-size:14pt;display:inline-block;">
+        <p>Avatar<br><img class="avatarButton-image" src="img/avatar/avatar_cuteawil.png" style="display:inline;horizontal-align:left;vertical-align:middle;display:inline;
+        padding:3%;align:right;"><br>Cute Awil</p>
       </button>
-      <button class="avatarButton" style="width:47%;font-size:18pt;display:inline-block;">
-        <p>Sidekick<img class="avatarButton-image" src="img/avatar/avatar_girwil.png" style="display:inline;vertical-align:middle;display:inline;
+      <button class="avatarButton" style="width:30%;font-size:14pt;display:inline-block;">
+        <p>Sidekick<br><img class="avatarButton-image" src="img/avatar/avatar_girwil.png" style="display:inline;vertical-align:middle;display:inline;
         padding:3%;align:right;"><br>Girwil</p>
       </button>
-      <button class="avatarButton" style="width:47%;font-size:18pt;display:inline-block;">
-      <p>Ride<img class="avatarButton-image" src="img/avatar/avatar_rocketleague.png" style="display:inline;vertical-align:middle;display:inline;
+      <button class="avatarButton" style="width:30%;font-size:14pt;display:inline-block;">
+      <p>Ride<br><img class="avatarButton-image" src="img/avatar/avatar_rocketleague.png" style="display:inline;vertical-align:middle;display:inline;
         padding:3%;align:right;"><br>Spell Mobile</p>
       </button>
 
