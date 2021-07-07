727fcfd133e3cb41ef7ba26c0af6b3845dcfb8d9
Andrew <adllivings@gmail.com>
2021-07-06 21:38:14 -0600

    menu-player submenu

--- components/menu-player.js
+++ components/menu-player.js
@@ -1,14 +1,17 @@
 document.write(
   `
   <div class="user-menu" id="menu-player">
-      <button class="menuButton">
-        <img class="menuButton-image" src="img/avatar/avatar_cuteawil.png"><br>Spellbound
+      <button class="avatarButton" style="width:47%;font-size:18pt;display:inline-block;">
+        <p>Spellbound<img class="avatarButton-image" src="img/avatar/avatar_cuteawil.png" style="display:inline;horizontal-align:left;vertical-align:middle;display:inline;
+        padding:3%;align:right;"></p>
       </button>
-      <button class="menuButton">
-        <img class="menuButton-image" src="img/avatar/avatar_girwil.png"><br>Sidekick
+      <button class="avatarButton" style="width:47%;font-size:18pt;display:inline-block;">
+        <p>Sidekick<img class="avatarButton-image" src="img/avatar/avatar_girwil.png" style="display:inline;vertical-align:middle;display:inline;
+        padding:3%;align:right;"><br>Girwil</p>
       </button>
-      <button class="menuButton">
-      <img class="menuButton-image" src="img/avatar/avatar_rocketleague.png"><br>Ride
+      <button class="avatarButton" style="width:47%;font-size:18pt;display:inline-block;">
+      <p>Ride<img class="avatarButton-image" src="img/avatar/avatar_rocketleague.png" style="display:inline;vertical-align:middle;display:inline;
+        padding:3%;align:right;"><br>Spell Mobile</p>
       </button>
 
   </div>
