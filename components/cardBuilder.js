39d6e4a338a1af9c7a415efb21768a4e0e60c1b4
Gregry Livingston <livingsgb@gmail.com>
2021-07-08 16:22:16 -0700

    Improved cards - data connection

--- components/cardBuilder.js
+++ components/cardBuilder.js
@@ -39,10 +39,11 @@ var nextCard;
 var scroller =   document.getElementById("cards").parentElement;
 var myTarget = undefined;
 scroller.addEventListener('scroll', function() {
-  if (myTarget!==undefined){
-  if(myTarget.getBoundingClientRect().top <= 500){
-    addCardsToFeed();
-  }}
+  if (myTarget!==null){
+    console.log(myTarget.getBoundingClientRect().top);
+    if(myTarget.getBoundingClientRect().top <= 500){
+      addCardsToFeed();
+    }}
 })
 
 function addCardsToFeed(){
@@ -51,8 +52,10 @@ function addCardsToFeed(){
         nextCard = availableCards[availableCardIterator];
         (nextCard !== undefined)? nextCard.addToFeed():availableCardIterator=i;
     }
-    (nextCard !== undefined)?     myTarget = document.querySelector('#'+nextCard.type+nextCard.index):myTarget=undefined;
-
+    console.log(nextCard);
+    console.log(document.querySelector('#'+nextCard.docId));
+    (nextCard !== undefined)? myTarget = document.querySelector('#'+nextCard.docId):myTarget=undefined;
+    console.log(myTarget);
 }
 
 
@@ -68,52 +71,83 @@ class card {
     this.badgeImg = "img/Badge/"+this.badge +"4.png";
     this.characterImg = "img/Character/" +this.Character + ".png";
     this.mediaType = "Image";
+    this.docId = this.type + this.index;
+    ( brains[difficultyLevels.indexOf(this.Difficulty)] !== undefined ) ? this.brains = brains[difficultyLevels.indexOf(this.Difficulty)]: this.brains="";
     this.cardHtml = this.makeCardHtml();
   }
   makeCardHtml(){
       return  `
-        <div class="card playerStyles" id="Challenge${this.index}">
+        <div class="card playerStyles" id="${this.docId}">
 
           <div class="card-type">
             <div>${this.type}</div>
           </div>
             ${this.makeCardHeader()}
-
-
-          <div class="card-section playerStyles">
-
-            <p>${this["Description"]}</p>
-          </div>
-          <div class="card-section playerStyles">
-            Showcase
-          </div>
-          <div class="card-section playerStyles">
-           ${this.makeCardProgressBar()}
-            Submit
-          </div>
-
-          <div class="card-description">
-
-          <div style="font-size:1.65em;">My Great Monologue</div>
-                <div>
-                    <button class="little-button">3 <img src='img/menu/menu_geniusshop.png' height='32px'> | :) | Emoticons</button>
-
-                    <div class="playerWidget" onclick="loadMenu('mmenu')" style="width:10em !important;">
-                                <img class="playerWidget-image" src="img/avatar/avatar_cuteawil.png">
-                                <div class="playerWidget-name" style="font-size:.75em;width:80%">Spellbound on 09/27/21</div>
-                    </div>
-                </div>
-
-
-
-
-          </div>
+            ${this.makeCardInnerContent()}
           ${this.makeCardFooter()}
         </div>
       `;
   }
   addToFeed(){
         document.getElementById("cards").innerHTML+=this.cardHtml;
+  }
+  makeCardInnerContent(){
+
+    switch (this.type){
+      case "Challenge":
+                return `
+                        <div class="card-section playerStyles">
+
+                          <p>${this["Description"]}</p>
+                        </div>
+                        <div class="card-section playerStyles" style="vertical-align:top;height:14em;">
+                          <div style="width:50%;border-right:2px solid white;display:inline-block;height:14em;vertical-align:top;">
+                              <div style="width:100%;height:45%;border-bottom:1px black solid">Showcase Item 1</div>
+                              <div style="width:50%;height:41%;display:inline-block;border-right:1px black solid;">Showcase Item 2</div>
+                              <div style="width:47%;height:41%;display:inline-block;border-left:1px black solid;">Showcase Item 3</div>
+                              <div style="width:100%;height:15%;text-align:center;">Showcase</div>
+
+                          </div>
+                          <div style="width:45%;display:inline-block;">
+                            Club Leaderboard<br>
+                            <img src="img/Club/korora.png" width="80%;"><br>
+                            Korora
+                          </div>
+                        </div>
+
+                        <div class="card-section playerStyles">
+                            You have not completed this challenge.
+                            <br>
+                             ${this.makeCardProgressBar()}
+                              <button class="menuButton"><h2>Submit</h2></button>
+                        </div>
+                        <div class="card-section playerStyles">
+                          Link to a resource
+                          <button>More Resources</button>
+                        </div>
+              `
+              break
+
+          case "Showcase":
+              return makeCardOwnershipWidet()
+          case "Event":
+              return "hello world Events"
+          default:
+              return "this card does not have an inner content type"
+      }
+  }
+  makeCardOwnershipWidget(){
+    return `<div class="card-description">
+
+    <div style="font-size:1.65em;">My Great Monologue</div>
+          <div>
+              <button class="little-button">3 <img src='img/menu/menu_geniusshop.png' height='32px'> | :) | Emoticons</button>
+
+              <div class="playerWidget" onclick="loadMenu('mmenu')" style="width:10em !important;">
+                          <img class="playerWidget-image" src="img/avatar/avatar_cuteawil.png">
+                          <div class="playerWidget-name" style="font-size:.75em;width:80%">Spellbound on 09/27/21</div>
+              </div>
+          </div>`
   }
   makeCardHeader(){
     return `<div class="card-header">
@@ -125,7 +159,7 @@ class card {
       </div>
       <img class="card-header-img" src=${this.badgeImg}>
       <div class="" style="width:35%;display:inline-block;text-align:left">${this.Character}</div>
-      <div class="" style="width:23%;display:inline-block;">${brains[difficultyLevels.indexOf(this.Difficulty)]}</div>
+      <div class="" style="width:23%;display:inline-block;">${this.brains}</div>
       <div class="" style="width:35%;display:inline-block;text-align:right;">${this.badge.replace("_"," ")}</div>
 
 
@@ -137,6 +171,7 @@ class card {
     <div class="card-footer">
       <div class="card-footer-left"></div>
       <div class="card-footer-right">
+      <button class="little-button">+ React</button><button class="little-button">+ Favorite</button>
           <button class="little-button">+ React</button><button class="little-button">+ Award</button>
           <button class="little-button">+ Share</button><button class="little-button">...</button>
       </div>
