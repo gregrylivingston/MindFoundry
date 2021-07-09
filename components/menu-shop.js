ea001b51922c2d4e04a689fc7d708e5d548c5cb0
Gregry Livingston <livingsgb@gmail.com>
2021-07-09 11:30:03 -0700

    New Data

--- components/menu-shop.js
+++ components/menu-shop.js
@@ -23,29 +23,23 @@ document.write(
               <button class="selectButton">
                 <div class="playerStyles" style="width:50%;height:1em"></div>
                 2/10
+                </button>
+              <button class="menuButton">
+                React
               </button>
-            </div>
+              <button class="selectButton">
+              <img src="img/avatar/avatar_builderawil.png" height="32px">
+                Award
+              </button>
 
 
-
-            Reactions
-                <div class="card-section playerStyles" style="width:96%;">
-                  <button class="menuButton">
-                    React
-                  </button>
-                  <button class="selectButton">
-                  <img src="img/avatar/avatar_builderawil.png" height="32px">
-                    Award
-                  </button>
+            <button class="menuButton" style="width:48%;font-size:1.2em;">
+              Tokens
+              2 <img src="img/token_league.png" style="height:1em">
+            </button>
+        </div>
 
 
-                      <button class="menuButton" style="width:48%;font-size:2em;">
-                        Tokens
-                        2 <img src="img/token_league.png" style="height:1em">
-                      </button>
-
-
-                    </div>
 
 
 
@@ -53,21 +47,17 @@ document.write(
                     Prizes
                         <div class="card-section playerStyles" style="width:96%;">
                           <button class="menuButton">
-                            <img src="img/avatar/avatar_builderawil.png" height="32px">
-                            2/10
+                            Community Work
                           </button>
                           <button class="selectButton">
-                          <img src="img/avatar/avatar_builderawil.png" height="32px">
+                            Club Gear
+                          </button>
+                          <button class="selectButton">
+                            Mind Foundry Swag
 
-                            2/10
                           </button>
                           <button class="selectButton">
-                              <i>Master Builders</i>
-                            2/10
-                          </button>
-                          <button class="selectButton">
-                            <div class="playerStyles" style="width:50%;height:1em"></div>
-                            2/10
+                            Megaprizes
                           </button>
                         </div>
           </div>
