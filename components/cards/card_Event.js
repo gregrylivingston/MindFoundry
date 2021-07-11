0000b275f46fee54cf789ffaaafd30aaf32dd4b6
Gregry Livingston <livingsgb@gmail.com>
2021-07-11 10:20:18 -0700

    better event cards

--- components/cards/card_Event.js
+++ components/cards/card_Event.js
@@ -29,29 +29,43 @@ class event_card extends card{
   hEventList(){
       return`
       <h3>Event List</h3>
-
             ${this.makeEvents()}
-
-
        `
   }
   makeEvents(){
         this.events = "";
         this.eventsInBadge.forEach(x=>{
+            var myDays = "";
+            if ( x[data["EventKey"].indexOf("eventType")] == "League"){
+                myDays = "On " + x[data["EventKey"].indexOf("Days")];
+                myDays = myDays.replace(",", "s and ")+"s ";
+                myDays +=  "at " + x[data["EventKey"].indexOf("startTimeCentral")];
+            } else {
+                myDays = "On " + x[data["EventKey"].indexOf("dateStart")] + " at " +  x[data["EventKey"].indexOf("startTimeCentral")];
+            }
+
+            //data on number of sessions
+//                  ${x[data["EventKey"].indexOf("Sessions")]} ${x[data["EventKey"].indexOf("SessionsLength")]} Sessions
+
             this.events+=`
                 <div class="card-section playerStyles">
-                  ${x[data["EventKey"].indexOf("eventTitle")]}
+                  <h3 style="width:100%;display:inline-flex;">
+                        <div style="width:70%">${x[data["EventKey"].indexOf("eventTitle")]}</div>
+                        <button class="selectButton" style="width:29%;">
+                          <div>Join</div><br>
+                          <div>1 <img src="img/token_${x[data["EventKey"].indexOf("eventType")]}.png" height="40px"></div>
+                        </button>
+                  </h3>
                   <br>
                   ${x[data["EventKey"].indexOf("Task")]}
                   <br>
-                  ${x[data["EventKey"].indexOf("Sessions")]} Sessions
+                  ${myDays}
                   <br>
-                  ${x[data["EventKey"].indexOf("SessionsLength")]} Minute Sessions
+                  Host: ${x[data["EventKey"].indexOf("host")]}
 
-                  <button class="half-button">
-                    <div>Join</div><br>
-                    <div>1 <img src="img/token_league.png" height="40px"></div>
-                  </button>
+
+
+
                 </div>
             `
         })
@@ -62,6 +76,7 @@ class event_card extends card{
           <div class="card-footer">
             <div class="card-footer-left">
                   <button class="topnav-button playerStyles"><img src="img/menu/favorite_unchecked.png"></button>
+                  <button class="topnav-button playerStyles"><img src="img/menu/react.png"></button>
 
                   </div>
             <div class="card-footer-right">
