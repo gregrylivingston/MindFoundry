5137d3c5af6dc329d133bf8057f15ca35db8e57f
Gregry Livingston <livingsgb@gmail.com>
2021-07-15 11:30:19 -0700

    New start page

--- js/initScreen.js
+++ js/initScreen.js
@@ -1,32 +1,45 @@
 
+
+
+
+//New Player
 var newplayer= data["Player"][0];
-document.getElementById("user_options_new").innerHTML+=`
-    <div class="card-section" style="max-height:2em;">
-    <button style="width:100%" onclick="intialize('${newplayer[0]}')" class="playerStyles">${widget_player(data["Player"][0])}</button>
-    </div>`;
+
+
+function init_getKorora(){
+//Korora
+document.getElementById("user_options_1").innerHTML = "<h3>Select a Player - Club Korora</h3>";
 
   data["Player"].filter(x=>x[data["PlayerKey"].indexOf("club")]=="Korora").forEach(x=>{
     if ( x[data["PlayerKey"].indexOf("userType")] !== "Player")
-      {document.getElementById("user_options_korora").innerHTML += "<br><h3>" + x[data["PlayerKey"].indexOf("userType")]} + "</h3>";
+      {document.getElementById("user_options_1").innerHTML += "<br><h3>" + x[data["PlayerKey"].indexOf("userType")]} + "</h3>";
 
-      document.getElementById("user_options_korora").innerHTML+=`
-          <div class="card-section" style="max-height:2em;">
-          <button style="width:100%" onclick="intialize('${x[0]}')" class="playerStyles">${widget_player(x)}</button>
+      document.getElementById("user_options_1").innerHTML+=`
+          <div class="card-section" style="max-height:2em;" onclick="intialize('${x[0]}')">
+            ${widget_player(x)}
           </div>
       `;
   });
+}
 
-    data["Player"].filter(x=>x[data["PlayerKey"].indexOf("club")]=="Headstart Academy").forEach(x=>{
-        x[data["PlayerKey"].indexOf("club")] = "Piwa";
-        if ( x[data["PlayerKey"].indexOf("userType")] !== "Player")
-          {document.getElementById("user_options_piwa").innerHTML += "<br><h3>" + x[data["PlayerKey"].indexOf("userType")]} + "</h3>";
+function init_getHeaderstart(){
+  document.getElementById("user_options_1").innerHTML = "<h3>Select a Player - Headstart Academy</h3>";
 
-        document.getElementById("user_options_piwa").innerHTML+=`
-            <div class="card-section" style="max-height:2em;">
-            <button style="width:100%"  onclick="intialize('${x[0]}')" class="playerStyles">${widget_player(x)}</button>
-            </div>
-        `;
-    });
+  //Headstart
+      data["Player"].filter(x=>x[data["PlayerKey"].indexOf("club")]=="Headstart Academy").forEach(x=>{
+          x[data["PlayerKey"].indexOf("club")] = "Piwa";
+          if ( x[data["PlayerKey"].indexOf("userType")] !== "Player")
+            {document.getElementById("user_options_1").innerHTML += "<br><h3>" + x[data["PlayerKey"].indexOf("userType")]} + "</h3>";
+
+          document.getElementById("user_options_1").innerHTML+=`
+              <div class="card-section" onclick="intialize('${x[0]}')">
+                  ${widget_player(x)}
+              </div>
+          `;
+      });
+
+}
+
 
   function intialize(p){
     ( p !== newplayer) ? data["Player"].unshift( data["Player"].find(x=>x[0]==p) ) : '' ;
@@ -34,3 +47,42 @@ document.getElementById("user_options_new").innerHTML+=`
     init();
 
   }
+
+
+
+
+  document.getElementById("user_options_0").innerHTML=`
+        <div class="card-section">
+          <h1>Mind Foundry 2021 Technical Demo</h1>
+        </div>`;
+
+  document.getElementById("user_options_1").innerHTML=`
+        <div class="card-section">
+          <h2>Select Demo Type</h2>
+          <h3>New User</h3>
+          <div class="playerStyles" onclick="intialize('${newplayer[0]}')">
+            ${widget_player(data["Player"][0])}
+          </div>
+          <h3>Online Club</h3>
+          <div class="playerStyles" onclick="init_getKorora()" style="padding:1em;">
+            Club Korora
+          </div>
+
+          <h3>Private Club</h3>
+          <div class="playerStyles" onclick="init_getKorora()" style="padding:1em;">
+            Headstart Academy
+          </div>
+        </div>`;
+
+  document.getElementById("user_options_2").innerHTML=`
+        <div class="card-section" style="text-align:left;">
+          <h3>Features</h3>
+          <div>Mobile First Designs</div>
+          <div>Improved curriculum and resource structures</div>
+          <div>Improved user widgets</div>
+          <div>Improved challenge cards</div>
+          <div>Full club functionality and leaderboards</div>
+
+          <div>Mobile First Designs</div>
+
+        </div>`;
